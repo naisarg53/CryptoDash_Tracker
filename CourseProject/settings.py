6b3075382c3e3163cf64e345ca88b9bc0dc6fe8d
Naisarg Pandya <59700346+naisarg53@users.noreply.github.com>
2022-03-11 23:11:09 -0500

    Update settings.py
    Hide data

--- CourseProject/settings.py
+++ CourseProject/settings.py
@@ -133,8 +133,8 @@ LOGIN_REDIRECT_URL = "index"
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'naisarg.pandya8853@gmail.com'
-EMAIL_HOST_PASSWORD = 'jaime9825'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Your Password'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
